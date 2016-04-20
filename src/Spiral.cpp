15d016f758f260a1e32b1b4f614fcc0019b969ea
Pavankanagarla <Pavankanagarla@missionrnd.com>
2016-04-20 21:44:17 +0530

    Submit Commit

--- src/Spiral.cpp
+++ src/Spiral.cpp
@@ -33,8 +33,38 @@ Note : Check the function Parameters ,Its a double pointer .
 
 #include "stdafx.h"
 #include<stdlib.h>
-
+void spiral_row( int **input_array, int k, int spc, int epc, int spr, int epr, int* resultset){
+	int i;
+	if (spc <= epc && spr <= epr){
+		for (i = spc; i <= epc; i++){
+			resultset[k] = input_array[spr][i];
+			k++;
+		}
+		for (i = spr + 1; i <= epr; i++){
+			resultset[k] = input_array[i][epc];
+			k++;
+		}
+		for (i = epc - 1; i >= spc; i--){
+			resultset[k] = input_array[epr][i];
+			k++;
+		}
+		for (i = epr - 1; i > spr && epc != 0; i--){
+			resultset[k] = input_array[i][spc];
+			k++;
+		}
+		spiral_row(input_array, k, spc + 1, epc - 1, spr + 1, epr - 1, resultset);
+	}
+}
 int *spiral(int rows, int columns, int **input_array)
 {
-	return NULL;
+	int starting_point_columns = 0;
+	int ending_point_columns = columns - 1;
+	int starting_point_row = 0;
+	int ending_point_row = rows - 1;
+	int k = 0;
+	int *result = (int*)malloc((rows * columns) * sizeof(int));
+	if (rows <= 0 || columns <= 0 || input_array == NULL) 
+		return NULL;
+	spiral_row(input_array,k, starting_point_columns,ending_point_columns, starting_point_row,ending_point_row,result);
+	return result;
 }
